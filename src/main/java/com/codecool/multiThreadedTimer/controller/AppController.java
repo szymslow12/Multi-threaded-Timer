2d90c5cd0382cd1de321758de679817d8122a2e3
szymslow12 <szym.slow12@gmail.com>
2019-01-19 16:41:38 +0100

    add handleUserChoice, rename startTimer -> handleAction

--- src/main/java/com/codecool/multiThreadedTimer/controller/AppController.java
+++ src/main/java/com/codecool/multiThreadedTimer/controller/AppController.java
@@ -1,5 +1,6 @@
 package com.codecool.multiThreadedTimer.controller;
 
+import com.codecool.multiThreadedTimer.model.StartTimer;
 import com.codecool.multiThreadedTimer.model.UserInputs;
 
 public class AppController {
@@ -7,6 +8,7 @@ public class AppController {
     private boolean flag = false;
     private UserInputs userInputs;
     private String userChoice;
+    private int timerNumber = 0;
 
     public AppController(UserInputs userInputs) {
         this.userInputs = userInputs;
@@ -20,23 +22,45 @@ public class AppController {
 
     public synchronized void actionChoice() {
         try {
-            if (!flag) {
-                alert("Enter what you want to do?\n\t" +
-                        "'start timerName' - start new timer\n\t" +
-                        "'end timerName' - ends timer");
-                userChoice = userInputs.getValue();
-                flag = true;
+            if (flag) {
                 wait();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        alert("Enter what you want to do?\n\t" +
+                "'start timerName' - start new timer\n\t" +
+                "'end timerName' - ends timer");
+        userChoice = userInputs.getValue();
+        flag = true;
+        notify();
     }
 
 
-    public synchronized void startTimer() {
-        if (flag) {
+    public synchronized void handleAction() {
+        if (!flag) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
+    }
+
+
+    private void handleUserChoice() {
+        String[] splitedUserChoice = userChoice.split(" ");
+        String action = splitedUserChoice[0];
+        String timerName = splitedUserChoice[1];
+        if (action.equalsIgnoreCase("start")) {
+            new Thread(new StartTimer(System.currentTimeMillis(), timerName, this), timerName).start();
+            flag = false;
+            notify();
+        } else if (action.equalsIgnoreCase("end")) {
+            alert("Ended");
+            flag = false;
+            notify();
         }
     }
 
