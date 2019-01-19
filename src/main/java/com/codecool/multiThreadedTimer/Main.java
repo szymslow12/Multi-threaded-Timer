b9c74cbaee7c9a42b7bf26d474761ee819e03ae6
szymslow12 <szym.slow12@gmail.com>
2019-01-19 20:12:13 +0100

    clean main in Main

--- src/main/java/com/codecool/multiThreadedTimer/Main.java
+++ src/main/java/com/codecool/multiThreadedTimer/Main.java
@@ -1,8 +1,15 @@
 package com.codecool.multiThreadedTimer;
 
+import com.codecool.multiThreadedTimer.controller.AppController;
+import com.codecool.multiThreadedTimer.model.ActionChoice;
+import com.codecool.multiThreadedTimer.model.ActionHandler;
+import com.codecool.multiThreadedTimer.model.UserInputs;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        AppController controller = new AppController(new UserInputs());
+        new ActionChoice(controller);
+        new ActionHandler(controller);
     }
 }
