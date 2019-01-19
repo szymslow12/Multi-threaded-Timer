c70f286815adca4b2a221bc92400b55989007b05
szymslow12 <szym.slow12@gmail.com>
2019-01-19 20:27:35 +0100

    pass View object as argument to new AppController

--- src/main/java/com/codecool/multiThreadedTimer/Main.java
+++ src/main/java/com/codecool/multiThreadedTimer/Main.java
@@ -4,11 +4,12 @@ import com.codecool.multiThreadedTimer.controller.AppController;
 import com.codecool.multiThreadedTimer.model.threads.ActionChoice;
 import com.codecool.multiThreadedTimer.model.threads.ActionHandler;
 import com.codecool.multiThreadedTimer.model.UserInputs;
+import com.codecool.multiThreadedTimer.view.View;
 
 public class Main {
 
     public static void main(String[] args) {
-        AppController controller = new AppController(new UserInputs());
+        AppController controller = new AppController(new UserInputs(), new View());
         new ActionChoice(controller);
         new ActionHandler(controller);
     }
