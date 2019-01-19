8165c47674c33c5b8cc7ea248f5179384b2c1d18
szymslow12 <szym.slow12@gmail.com>
2019-01-19 16:18:11 +0100

    impl actionChoice, add getUserChoice/startTimer()

--- src/main/java/com/codecool/multiThreadedTimer/controller/AppController.java
+++ src/main/java/com/codecool/multiThreadedTimer/controller/AppController.java
@@ -6,14 +6,36 @@ public class AppController {
 
     private boolean flag = false;
     private UserInputs userInputs;
+    private String userChoice;
 
     public AppController(UserInputs userInputs) {
         this.userInputs = userInputs;
     }
 
 
-    public void run() {
-        if (!flag) {
+    public String getUserChoice() {
+        return userChoice;
+    }
+
+
+    public synchronized void actionChoice() {
+        try {
+            if (!flag) {
+                alert("Enter what you want to do?\n\t" +
+                        "'start timerName' - start new timer\n\t" +
+                        "'end timerName' - ends timer");
+                userChoice = userInputs.getValue();
+                flag = true;
+                wait();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public synchronized void startTimer() {
+        if (flag) {
 
         }
     }
