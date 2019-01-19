6f873d8d01780a32d68de1b2332113d75ee2d4ba
szymslow12 <szym.slow12@gmail.com>
2019-01-19 16:13:17 +0100

    rename to ActionChoice add run()

---
+++ src/main/java/com/codecool/multiThreadedTimer/model/ActionChoice.java
@@ -0,0 +1,18 @@
+package com.codecool.multiThreadedTimer.model;
+
+import com.codecool.multiThreadedTimer.controller.AppController;
+
+public class ActionChoice implements Runnable {
+
+    private AppController controller;
+
+    public ActionChoice(AppController controller) {
+        this.controller = controller;
+        new Thread(this, "ActionChoice").start();
+    }
+
+    @Override
+    public void run() {
+        controller.getUserChoice();
+    }
+}
